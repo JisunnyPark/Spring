cae9aa3508783c394bf4b9fc12f337450316811f
Jiseon Park <79893752+JisunnyPark@users.noreply.github.com>
2021-06-24 10:17:17 +0900

    Spring_20210624

---
+++ 05_Spring_DI_Annotation/src/main/java/di_annotation/AppleSpeaker.java
@@ -0,0 +1,18 @@
+package di_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component("apple")
+public class AppleSpeaker implements Speaker {
+	public AppleSpeaker() {
+		System.out.println("-->> AppleSpeaker 생성");
+	}
+	
+	public void volumnUp() {
+		System.out.println("-->> AppleSpeaker - 소리 크게");
+	}
+	
+	public void volumnDown() {
+		System.out.println("-->> AppleSpeaker - 소리 작게");
+	}
+}
